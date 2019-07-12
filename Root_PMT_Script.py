7395ae330860e19872d24835cfc4d2fc43828323
Michael Williams <51758097+mrw7816@users.noreply.github.com>
2019-07-11 20:34:16 -0400

    Update Root_PMT_Script.py

--- Root_PMT_Script.py
+++ Root_PMT_Script.py
@@ -19,6 +19,23 @@ print(Graph_DarkPlot)
 DarkPlot_canvas = r.TCanvas('Dark_Canvas')
 Graph_DarkPlot.Draw()
 DarkPlot_canvas.SaveAs('Dark_Count_Long')
+
+#These three lines grap the x and y arrays and their errors from the root file
+DarkX_buff = Graph_DarkPlot.GetX()
+DarkY_buff = Graph_DarkPlot.GetY()
+Yerror_buff = Graph_DarkPlot.GetEY()
+# These two lines provide the pythonization of the buffers into actual doubles
+DarkX_buff.SetSize(Graph_DarkPlot.GetN())
+DarkY_buff.SetSize(Graph_DarkPlot.GetN())
+Yerror_buff.SetSize(Graph_DarkPlot.GetN())
+#These two create the actual arrays
+DarkX = ar.array('d' ,DarkX_buff)
+DarkY = ar.array('d' ,DarkY_buff)
+DarkYerror = ar.array('d', Yerror_buff)
+
+
+plt.errorbar(DarkX,DarkY,yerr=DarkYerror)
+plt.show()
 #points = r.TH1D("dark rate", "Dark Count Rate", 0,100)
 
 #for entryNum in range(0,tree.GetEntries()):  #fills histo
